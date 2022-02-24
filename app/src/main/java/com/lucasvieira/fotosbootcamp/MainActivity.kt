f15ce0be98ed1e110a5eeba6da29f7a8dfc67abb
Lucas Vieira <lvieirastos@gmail.com>
2022-02-24 16:34:58 -0300

    Lógica criada

--- app/src/main/java/com/lucasvieira/fotosbootcamp/MainActivity.kt
+++ app/src/main/java/com/lucasvieira/fotosbootcamp/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.lucasvieira.fotosbootcamp
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        pick_button.setOnClickListener(View.OnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                       == (PackageManager.PERMISSION_DENIED )){
+                    val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permission, PERMISSION_CODE)
+                }
+                else {
+                    pickImageFromGallery()
+                }
+            }
+            else {
+                pickImageFromGallery()
+            }
+        }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImageFromGallery()
+                } else {
+                    Toast.makeText(this, "Permissão negada", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun pickImageFromGallery() {
+        TODO("Not yet implemented")
+    }
+
+    companion object{
+        private val PERMISSION_CODE = 1000
+        private val IMAGE_PICK_CODE = 1001
+
     }
 }
